78df07503b19278e2e947a5577258057430ee05f
Fredrik Olsson <freol@outlook.com>
2023-11-16 10:54:04 +0100

    Fixes the relative import problems described in issue 3. Also moves code generation logic for brefv into a centralized script for all languages. Cleaning up temporary workaround in ouster script

---
+++ brefv/generate_code.sh
@@ -0,0 +1,33 @@
+# This file generates code for all brefv implementations
+
+# Enables globstar feature of bash
+shopt -s globstar
+
+## Python
+	# Clean up all old generated files
+	echo "Cleaning up old files..."
+	rm -rf python/brefv/**/*_pb2*
+
+	# Copy tags.yaml (forcing an overwrite)
+	echo "Copying tags.yaml..."
+	cp -rf tags.yaml python/brefv/tags.yaml 
+
+	# Generate code for core.proto
+	echo "Generating code for core.proto..."
+	protoc \
+		--python_out=python/brefv \
+		--proto_path . \
+		core.proto
+
+	# Generate code for payloads
+	echo "Generating code for payloads..."
+	protoc \
+		--python_out=python/brefv/payloads \
+		--proto_path=payloads \
+		--descriptor_set_out=python/brefv/payloads/protobuf_file_descriptor_set.bin \
+		--include_imports \
+		payloads/**/*.proto
+
+	# Ensuring the generated code for foxglove is importable as a subpackage
+	echo "Change imports to relative..."
+	sed -E -i 's/from foxglove import/from . import/g' python/brefv/payloads/foxglove/*_pb2.py
