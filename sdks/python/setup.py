c6f091caaee6dbd65cc5de21e48d963538b5a218
Fredrik Olsson <freol@outlook.com>
2024-04-09 15:35:16 +0200

    Adding test job for the Docker image

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name="keelson",
-    version="0.3.0-pre.15",
+    version="0.3.0",
     license="Apache License 2.0",
     description="A python Software Development Kit for keelson",
     long_description=read("README.md"),
