6ec72986cc455fec8a1267b635b3efcd13e57176
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-04-12 21:24:06 +0200

    version error fixed

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name="keelson",
-    version="0.3.1",
+    version="0.3.1-pre.2",
     license="Apache License 2.0",
     description="A python Software Development Kit for keelson",
     long_description=read("README.md"),
