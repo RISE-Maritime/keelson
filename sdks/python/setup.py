dce256d8fc6d8e70ced3173c332f55e8863fd393
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2024-05-13 11:22:51 +0200

    version bump

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name="keelson",
-    version="0.3.1-pre.22",
+    version="0.3.1-pre.23",
     license="Apache License 2.0",
     description="A python Software Development Kit for keelson",
     long_description=read("README.md"),
