dfd3fdbccfe19b3aa0a180c6ba10d3221d7fdde7
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-08-09 12:16:18 +0200

    Target added  0.3.7-pre.45

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name="keelson",
-    version="0.3.7-pre.45",
+    version="0.3.7-pre.46",
     license="Apache License 2.0",
     description="A python Software Development Kit for keelson",
     long_description=read("README.md"),
