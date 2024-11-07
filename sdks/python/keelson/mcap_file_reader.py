14e1dbb5ef0ad434dc81b49ea42c05f388b7ea51
martinsa-ri <150011860+martinsa-ri@users.noreply.github.com>
2024-11-07 15:19:16 +0100

    Added example reading axis camera and boat radar
    First version with axis camera, works the same as webcam
    First version with boat radar, uses pointcloud protobuf but needs some transformation
    Change example file to be more descriptive

--- sdks/python/keelson/mcap_file_reader.py
+++ sdks/python/keelson/mcap_file_reader.py
@@ -14,8 +14,8 @@ class readMCAPiterator:
         self.reader = make_reader(self.file, decoder_factories=[DecoderFactory()])
         #register schema handlers, please add new ones here
         schema_handlers_schemabased = {
-            "foxglove.CompressedImage": self.handle_webcam_image, #webcam image
-            "foxglove.PointCloud": self.handle_aptiv_point_cloud, #e.g. os2 lidar AND aptiv radar
+            "foxglove.CompressedImage": self.handle_webcam_image, #webcam image OR axis-1 image
+            "foxglove.PointCloud": self.handle_aptiv_point_cloud, #e.g. os2 lidar AND aptiv radar AND boat radar
             "keelson.primitives.TimestampedBytes": self.handle_none, #e.g. raw aptiv radar data
             "keelson.compound.ImuReading": self.handle_none, #e.g. imu data from os2 is not implemented yet
         }
@@ -51,25 +51,32 @@ class readMCAPiterator:
 
     def handle_aptiv_point_cloud(self,proto_msg,channel,message):
         #TODO: at the moment very limited to float64, need to become more general
-        type_mapping = {
+        type_mapping1 = {
             8: 'd',  # double = 8 bytes = np.float64
-            4: 'f',  # float = 4 bytes = np.float32
+            7: 'f',  # float = 4 bytes = np.float32
             0: 'n/a',  # add more if needed
         }
+        type_mapping2 ={
+            8: np.float64,
+            7: np.float32,
+            0: np.float32,
+        }
         field_names = [field.name for field in proto_msg.fields]
-        format_string = '<' + ''.join(type_mapping[field.type] for field in proto_msg.fields)
+        field_types = [field.type for field in proto_msg.fields]
+        format_string = '<' + ''.join(type_mapping1[typ] for typ in field_types)
         segment_size = struct.calcsize(format_string)
         assert segment_size == proto_msg.point_stride
-        dtype = np.dtype([(name, np.float64) for name in field_names])
+        #dtype = np.dtype([(name, np.float64) for name in field_names])
+        dtype = np.dtype([(name, type_mapping2[typ]) for name,typ in zip(field_names,field_types)])
         structured_array = np.frombuffer(proto_msg.data, dtype=dtype)
-        cloud = structured_array.view(np.float64).reshape(-1, len(field_names))
-        #cast to float32 to save memory
-        cloud = cloud.astype(np.float32)
+        largest_dtype = np.float64 if 8 in field_types else np.float32
+        cloud = structured_array.view(largest_dtype).reshape(-1, len(field_names)) #what type to use, need largest datatype here?
+        #cloud = cloud.astype(np.float32), #cast to float32 to save memory, unsure if it helps, also: tuple or list?
         metadata = self.handle_metadata(proto_msg,channel,message)
         return channel.topic, cloud, field_names, metadata
 
     def handle_webcam_image(self,proto_msg,channel,message):
-        #TODO: call IOBytes and Image.open here instead
+        #TODO: call IOBytes and Image.open here instead?
         field_names = []
         img = proto_msg.data
         metadata = self.handle_metadata(proto_msg,channel,message)
--- sdks/python/keelson/read_protobuf_example.py
+++ sdks/python/keelson/read_protobuf_example.py
@@ -1,4 +1,5 @@
 import rerun as rr
+import os
 import numpy as np
 from itertools import islice
 from mcap_file_reader import readMCAPiterator
@@ -12,30 +13,37 @@ from io import BytesIO
 
 def plot_with_rerun(filename,topics,start,stop):
 	mcap_iter = islice(readMCAPiterator(filename, topics),start,stop)
-	rr.init("Incident_kayak_in_front_of_arriving_ferry",spawn=True)
+	log_name = os.path.basename(filename) #could be tailored
+	rr.init(log_name,spawn=True)
 	for topic, data, col_label, metadata in mcap_iter:
 		if len(data) > 0:
-			if topic == topics[0]:
+			if '1201' in topic:
+				data_conv = np.vstack((-data[:,0],data[:,1])).T
+				rr.log("radar/r1201",rr.Points2D(data_conv,radii=1.0))
+			elif 'aptiv' in topic:
 				#do some processing here
 				mask = np.logical_and(data[:,col_label.index('az_conf')] < 1,data[:,col_label.index('el_conf')] < 1) 
 				pc0 = data[mask,0:3]
 				rr.log("radar/pc_aptiv",rr.Points3D(pc0,radii=3.0))
 				rr.log("radar/final_size",rr.Scalar(pc0.shape[0]))
-			elif topic == topics[1]:	
+			elif 'usb' or 'axis-1' in topic:	
 				rr.log("camera/webcam",rr.Image(Image.open(BytesIO(data))))
-			elif topic == topics[2]:
+			elif 'os2' in topic:
 				rr.log("lidar/pc_os2",rr.Points3D(data[:,0:3],radii=1.0))
 			#TODO: add time from MCAP metadata
 
 
 #input examples
-filename = r"C:\Users\martinsa\RISE\EPA - RISE - Dokument\RISE\3. Data\2024-09-06_Fiskebäck\Incident_kayak_in_front_of_arriving_ferry\0902_lidar_aptiv_webcam.mcap"
-#filename = r"C:\Users\martinsa\RISE\EPA - RISE - Dokument\RISE\3. Data\2024-09-06_Fiskebäck\Incident_kayak_in_front_of_arriving_ferry\0902_radar_cam.mcap"
+#filename = r"C:\Users\martinsa\RISE\EPA - RISE - Dokument\RISE\3. Data\2024-09-06_Fiskebäck\Incident_kayak_in_front_of_arriving_ferry\0902_lidar_aptiv_webcam.mcap"
+filename = r"C:\Users\martinsa\RISE\EPA - RISE - Dokument\RISE\3. Data\2024-09-06_Fiskebäck\Incident_kayak_in_front_of_arriving_ferry\0902_radar_cam.mcap"
 topics = ["rise/v0/landkrabba-two/pubsub/point_cloud/radar/aptiv/0",
 		  "rise/v0/landkrabba-two/pubsub/compressed_image/usb",
+		  "rise/v0/landkrabba/pubsub/compressed_image/axis-1",
 		  "rise/v0/landkrabba-two/pubsub/point_cloud/lidar/os2/0",
 		  "rise/v0/landkrabba/pubsub/point_cloud/1201"] #1202 och 1201 does not work yet
-start = 6000 #6000
-stop = 13000 #13000
+start = 17000 #6000, 17000
+stop = 38000 #13000, 38000
+
+#TODO: start, stop in ticks should be calculated from time to synchronize with other files
 
 plot_with_rerun(filename,topics,start,stop)
