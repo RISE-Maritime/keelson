bfb09be7662270c8211f4f0394fad00f931c7952
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2025-03-01 16:23:28 +0100

    0.3.9rc1

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name="keelson",
-    version="0.3.8",
+    version="0.3.9rc1",
     license="Apache License 2.0",
     description="A python Software Development Kit for keelson",
     long_description=read("README.md"),
