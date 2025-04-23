86b0c4d6268ca568f4cb4f5f7ddffa45de993adc
Fredrik Olsson <freol@outlook.com>
2025-04-23 13:54:50 +0200

    Removing generated code and reverting foxglove proto files to original.

--- sdks/python/keelson/interfaces/Configurable_pb2.pyi
+++
@@ -1,7 +0,0 @@
-from google.protobuf import empty_pb2 as _empty_pb2
-from google.protobuf import struct_pb2 as _struct_pb2
-import Response_pb2 as _Response_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
